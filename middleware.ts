edbd29fb47f98a97c0177870d14d51d5f16a50e5
Anderson <avictoral@gmail.com>
2024-07-10 15:44:19 -0400

    created api assistance create endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isDashboardRoute = createRouteMatcher(["/(.*)"]);
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 export default clerkMiddleware((auth, request) => {
   // Restrict dashboard routes to signed in users
-  // if (isDashboardRoute(request)) auth().protect();
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+  if (isDashboardRoute(request)) auth().protect();
+  // if (!isPublicRoute(request)) {
+  //   auth().protect();
+  // }
 });
 
 export const config = {
