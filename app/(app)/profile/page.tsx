fb1e34e39e9bced51d9b90407f7ddbb4ee0b6715
Anderson <avictoral@gmail.com>
2024-07-10 22:39:52 -0400

    added new components for profile scene

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -1,3 +1,4 @@
+import ProfileContainer from "@/components/ProfileContainer";
 import { prismadb } from "@/lib/prismadb";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
